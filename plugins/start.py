b618e7a2e1f8961b7043c94a6e4a6c15b8b2427f
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2024-03-12 12:29:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -127,7 +127,7 @@ async def not_joined(client: Client, message: Message):
                              url = client.invitelink),
                        InlineKeyboardButton(
                              text="Channel 2",
-                             url = "https://t.me/+ktgEDzxI7LlhNzk1"),
+                             url = "https://t.me/+JYpYAonmGzMwODFl"),
         ]
     ]
     try:
